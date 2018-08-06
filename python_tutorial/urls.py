c7cd8b4518d0d19b02d224aadf1442dec383d3cf
Shalice Moideen <shalice.moideen@marlabs.com>
2018-08-06 18:52:01 +0530

    Url modified

--- python_tutorial/urls.py
+++ python_tutorial/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # Invoke the home view in the tutorial app by default
     url(r'^$', views.home, name='home'),
     # Defer any URLS to the /tutorial directory to the tutorial app
-    url(r'^tutorial/', include('tutorial.urls', namespace='tutorial')),
+    url(r'^outlook/tutorial/', include('tutorial.urls', namespace='tutorial')),
     url(r'^admin/', admin.site.urls),
 ]
